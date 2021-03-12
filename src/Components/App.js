46cfc897591196f35835017abba7a7c3ef84b643
Ashutosh singh <54509476+Ashutosh-singh-au7@users.noreply.github.com>
2021-03-12 17:37:55 +0530

    Add files via upload

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React,{} from "react";
 import Login from "./login";
 import Register from "./register";
-
+import Header from "./Header";
+import {Router,Route} from "react-router-dom"
+import {history} from "../helpers/history";
 const App = ()=>{
+    // useEffect(()=>{
+    //     history.push("/register")
+    // })
+
     return(
-        <div>
-            <Login/>
-            <Register/>
-        </div>
+        <Router history={history} >
+            <div>
+                <Header/>
+                <div>
+                        <Route path={"/"} exact component={Login} />
+                        <Route path={"/register"} component={Register} />
+                </div>
+            </div>
+        </Router>
+        
     )
 }
 export default  App;
